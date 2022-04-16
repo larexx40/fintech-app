985f362a33bc1070a768f8dcb2acdcbee511bbe0
larexx40 <larexx40@gmail.com>
2022-04-16 10:26:06 +0100

    Reserve an account for user

--- controller/monnify.js
+++ controller/monnify.js
@@ -4,6 +4,7 @@ const axios = require('axios');
 const apiKey = process.env.MONNIFY_API_KEY
 const secretKey = process.env.MONNIFY_SECRET_KEY
 const baseUrl = process.env.MONNIFY_BASE_URL
+const contractCode = process.env.MONNIFY_CONTRACT_CODE
 
 // generate auth2.0 bearer token
 async function authenticate(){
@@ -20,8 +21,60 @@ async function authenticate(){
     
 }
 
+async function verifyBVN (){
+    const input = {
+        "bvn":"22222222226",
+        "name": "OLATUNDE JOSIAH OGUNBOYEJO",
+        "dateOfBirth": "27-Apr-1993",
+        "mobileNo": "08142223149"
+    }
 
+    const accessToken = await authenticate();
+    const headers = {
+        Authorization: 'Bearer ' + accessToken
+    }
+
+    const url = 'https://api.monnify.com/api/v1/vas/bvn-details-match'
+    const response = await axios.post(url, input, {headers});
+    const responseBody = response.data;
+    console.log(responseBody);
+}
+
+async function verifyAcc (){
+    const accessToken = await authenticate();
+    const headers = {
+        Authorization: 'Bearer ' + accessToken
+    }
+
+    const response = await axios.get(baseUrl + '/api/v1/disbursements/account/validate?accountNumber=0068687503&bankCode=232', null, {headers});
+    const responseBody = response.data;
+    console.log(responseBody);
+}
+
+async function reserveAcc () {
+    const input ={
+        "accountReference": "abc1234",
+        "accountName": "Test Reserved Account",
+        "currencyCode": "NGN",
+        "contractCode": contractCode,
+        "customerEmail": "test@tester.com",
+        "bvn": "21212121212",
+        "customerName": "John Doe", 
+        "getAllAvailableBanks": false,
+        "preferredBanks": ["035"]
+    }
+    const accessToken = await authenticate();
+    const headers = {
+        Authorization: 'Bearer ' + accessToken
+    }
+    const response = await axios.post(baseUrl + '/api/v2/bank-transfer/reserved-accounts', input, {headers});
+    const responseBody = response.data;
+    console.log(responseBody);
+}
 
 module.exports = {
     authenticate: authenticate,
+    verifyBVN: verifyBVN,
+    verifyAcc: verifyAcc,
+    reserveAcc:reserveAcc,
 }
