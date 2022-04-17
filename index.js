5f0310c1d95da02feff318bc577deba860da873d
larexx40 <larexx40@gmail.com>
2022-04-17 13:30:38 +0100

    twilio smsOtp

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const monnifyRouter = require('./routes/monnify');
 const userRouter = require('./routes/users');
 const africastalkingRouter = require('./routes/africastalking');
+const twilioRouter = require('./routes/twilio');
 
 const app = express();
 
@@ -10,7 +11,7 @@ app.use(bodyParser.json())
 app.use('/monnify', monnifyRouter );
 app.use('/user', userRouter);
 app.use('/africastalking', africastalkingRouter);
-
+app.use('/twilio', twilioRouter);
 
 const server = app.listen(3000, ()=>{
     console.log("Server created at port 3000");
