6e03f8413f1191a0cb4300c99d466f24b6ddbae9
larexx40 <larexx40@gmail.com>
2022-04-25 19:56:33 +0100

    edit twilio

--- controller/twilio.js
+++ controller/twilio.js
@@ -4,17 +4,20 @@ const authToken = process.env.TWILIO_AUTH;
 //Step1 twillo(setup)
 const client = require('twilio')(accountSid, authToken);
 
-client.verify.services.create({friendlyName: 'My First Verify Service'})
-.then(service => console.log({service: service.sid, status: "SENT"}));
+const twilio= (phoneNo, otp)=>{
+    const to = phoneNo
+    const code = otp
+}
 
 //above step return sid
 
 //step 2 create verification(send token)
-exports.sendSmsOtp = (req, res)=>{
+exports.sendSmsOtp = async (req, res)=>{
     try {
-       const sendOtp = await client.verify.services('VAe94857d2081c4407af51fa1a75a1313d')
+        const {to} = req.body
+       const sendOtp = await client.verify.services(`${process.env.TWILIO_SERVICEID}`)//VAead84de8656d7d20fd3bbda037d53da1
        .verifications
-       .create({to: req.body , channel: 'sms'})
+       .create({to , channel: 'sms'})
        const status = sendOtp.status;
        console.log(status);
        return res.status(200).json(status)
@@ -25,16 +28,38 @@ exports.sendSmsOtp = (req, res)=>{
 }
 
 //step3 verify received token
-exports.verifyOtp = (req, res)=>{
+exports.verifyOtp =async (req, res)=>{
     try {
-        const verifyOtp = client.verify.services('VAe94857d2081c4407af51fa1a75a1313d')
+        const {to, code} = req.body 
+        const verifyOtp = await client.verify.services('VAead84de8656d7d20fd3bbda037d53da1')
         .verificationChecks
-        .create({to: '+2348100529552', code: req.body})
+        .create({to, code})
         const status = verifyOtp.status;
         console.log(status);
         return res.status(200).json(status);        
     } catch (error) {
         console.log(error);
         return res.status(400).json(error);
+    }
+}
+
+async function otp(phoneNo, token){
+    sentOtp= ()=>{
+        const {to} = phoneNo
+        const sendOtp = await client.verify.services(`${process.env.TWILIO_SERVICEID}`)
+        .verifications
+        .create({to , channel: 'sms'})
+        const status = sendOtp.status;
+        return status
+    }
+
+    verifyOtp = ()=>{
+        const {to} = phoneNo
+        const {code}= token
+        const verifyOtp = await client.verify.services('VAead84de8656d7d20fd3bbda037d53da1')
+        .verificationChecks
+        .create({to, code})
+        const status = verifyOtp.status;
+        console.log(status);       
     }
 }
